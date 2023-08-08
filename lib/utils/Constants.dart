f823874a5e77cfcfc2dea509fe2dc1d53ee972ea
faysalzaman <faysalzaman00@gmail.com>
2023-08-08 02:59:47 -0700

    u

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Constants {
-  static String baseUrl = 'http://gs1ksa.org:3005/api/';
-  static String host = 'gs1ksa.org';
+  // static String baseUrl = 'http://gs1ksa.org:3005/api/';
+  // static String host = 'gs1ksa.org';
 
-  // static String baseUrl = 'http://37.224.47.116:7474/api/';
-  // static String host = '37.224.47.116:7474';
+  static String baseUrl = 'http://37.224.47.116:7474/api/';
+  static String host = '37.224.47.116:7474';
 
   static Future<dynamic> showLoadingDialog(BuildContext context) {
     return showDialog(
