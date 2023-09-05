2bb7faa7c12f4cb28889192612d32b2d65f06c97
faysalzaman <faysalzaman00@gmail.com>
2023-09-05 20:29:58 +0500

    Updated

--- lib/screens/WMSInventory/WMSInventoryScreen.dart
+++ lib/screens/WMSInventory/WMSInventoryScreen.dart
@@ -6,8 +6,6 @@ import 'package:alessa_v2/controllers/WMSInventory/GetDistinctMappedBarcodeBinLo
 import 'package:alessa_v2/controllers/WMSInventory/GetmapBarcodeDataByMultipleBinLocations.dart';
 import 'package:alessa_v2/models/GetmapBarcodeDataByBinLocationModel.dart';
 import 'package:dropdown_search/dropdown_search.dart';
-import 'package:multi_select_flutter/chip_display/multi_select_chip_display.dart';
-import 'package:multi_select_flutter/chip_field/multi_select_chip_field.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
 import 'package:multi_select_flutter/util/multi_select_list_type.dart';
